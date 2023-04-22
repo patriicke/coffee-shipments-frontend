d2f8543a22e43a67e1f23ca47ebdb38f6e309047
patriicke <patrickndayambaje4@gmail.com>
2023-04-22 21:01:31 +0200

    update(): borders

--- src/pages/trace/components/navbar/TraceNavBarComponent.tsx
+++ src/pages/trace/components/navbar/TraceNavBarComponent.tsx
@@ -1,13 +1,50 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useSelector } from 'react-redux';
 import { ICONS } from '~/assets/icons';
+import {
+    AdminContext,
+    AdminContextType,
+} from '~/core/provider/admin/AdminProvider';
 import { REDUX_STORE_TYPE } from '~/core/redux/store';
 
 export const TraceNavBarComponent: React.FC = () => {
     const { userData } = useSelector((state: REDUX_STORE_TYPE) => state.user);
 
+    const { toggleSidebar } = useContext<AdminContextType>(AdminContext);
+
     return (
-        <div className="flex h-20 w-full items-center justify-end border-opacity-40 bg-primary-500 bg-opacity-30">
+        <div className="flex h-20 w-full items-center justify-between border-opacity-40 bg-primary-500 bg-opacity-30 md:justify-end">
+            <button className="block pl-4 md:hidden" onClick={toggleSidebar}>
+                <svg
+                    width="28"
+                    height="28"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path
+                        d="M3 12H21"
+                        stroke="#6F4E37"
+                        stroke-width="1.5"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                    />
+                    <path
+                        d="M3 5H21"
+                        stroke="#6F4E37"
+                        stroke-width="1.5"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                    />
+                    <path
+                        d="M3 19H21"
+                        stroke="#6F4E37"
+                        stroke-width="1.5"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                    />
+                </svg>
+            </button>
             <div className="mr-10">
                 <div className="group relative">
                     <button>
